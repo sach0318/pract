93bc446d5e78ee306dbdba62219f824370ef4630
sach0318 <sachinyadv0318@gmail.com>
2025-05-12 12:37:27 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
-// import { useState } from 'react'
+import { useState, useCallback } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, seNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()~/{}";
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <div >
-        <h1 className="text-4xl text-center text-white">Password hello Generator</h1>
-      </div>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-black-500 bg-gray-800 ">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 bg-white">
+          <input type="text" value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="password"
+          readOnly />
+          <button className="outline-none bg-blue-700  py-0.5 shrink-0 px-3">copy</button>
+
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer text-white"
+             />
+             <label className="text-white"> length : {length}</label>
+          </div>
+
+        </div>
+        </div>
     </>
   );
 }
 
-export default App
+export default App;
