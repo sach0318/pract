63ad3b6d5a9d8651ecd642f0c3380b446a85b6d8
sach0318 <sachinyadv0318@gmail.com>
2025-05-12 10:46:40 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div >
+        <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
